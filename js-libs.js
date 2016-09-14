78c6968157d295abcd353bddac2e3acdda851277
AustinJackS <austinjack404@gmail.com>
2016-09-14 17:13:33 -0400

    Update js-libs.js

--- js-libs.js
+++ js-libs.js
@@ -3,10 +3,10 @@ function loadDoc(url) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
+      window.code = this.responseText;
     }
   };
   xhttp.open("GET", url, true);
   xhttp.send();
-  eval(url)
+  eval(code)
 }
